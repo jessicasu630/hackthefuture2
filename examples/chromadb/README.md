d50318dbd28aeb25866f389f7fb4e4f4b1e135e9
Asad Akbar <asadtheakbar@gmail.com>
2026-03-03 20:35:46 -0500

    chore: push all workspace changes

--- examples/chromadb/README.md
+++
@@ -1,3 +0,0 @@
-# ChromaDB integration
-
-This [tutorial](./Vectordb_with_chroma.ipynb) demonstrates how to use the Gemini API to create a vector database and retrieve answers to questions from the database using [ChromaDB](https://docs.trychroma.com/){:.external}, an open-source Python tool that creates embedding databases.
---
+++ src/utils/fr/navigation.ts
@@ -0,0 +1,34 @@
+﻿const navBarLinks: { name: string; url: string }[] = [];
+
+const footerLinks = [
+  {
+    section: 'Plateforme',
+    links: [
+      { name: 'Resolv DRS', url: '/fr' },
+      { name: 'Architecture', url: '#architecture' },
+      { name: "Cas d'usage", url: '#use-cases' },
+    ],
+  },
+  {
+    section: 'Entreprise',
+    links: [
+      { name: 'Helios Industrial Components', url: '#' },
+      { name: 'Contact', url: '#contact' },
+      { name: 'Confidentialite', url: '#' },
+    ],
+  },
+];
+
+const socialLinks = {
+  facebook: 'https://www.facebook.com/',
+  x: 'https://twitter.com/',
+  github: 'https://github.com/mearashadowfax/ScrewFast',
+  google: 'https://www.google.com/',
+  slack: 'https://slack.com/',
+};
+
+export default {
+  navBarLinks,
+  footerLinks,
+  socialLinks,
+};
